faac9827423e6ffd93cf3fa80b505248a1e66a3b
Callum Campbell <callum.campbell@sgept.org>
2024-03-24 12:08:11 +0000

    initial parsing done

---
+++ 2023/5/5.py
@@ -0,0 +1,62 @@
+import os
+import re
+
+os.chdir('2023/5')
+
+
+def read_file_as_list(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+            lines = [line.strip() for line in lines]
+            return lines
+    except FileNotFoundError:
+        print(f"File '{file_path}' not found.")
+        return []
+
+puzz_input_raw = read_file_as_list('ex_1.txt')
+
+def parse_puzz_input(puzz_input_raw):
+
+    # assumes it will be on line 0
+    seed_info = re.findall(string=puzz_input_raw[0], pattern=r'\d+')
+
+    puzz_input_no_seed_info = puzz_input_raw.copy()
+
+    # this should be dynamic
+    puzz_input_no_seed_info = puzz_input_no_seed_info[2:]
+
+    # get the types dynamically
+    correspondence_types_raw = [re.findall(string=x, pattern=r'\w+-to.+') for x in puzz_input_no_seed_info]
+
+    # remove empty elements
+    correspondence_types = [x[0] for x in correspondence_types_raw if x]
+
+    # dictionary of type: the correspondence numbers
+    correspondence_dict = dict()
+
+    # instantiate hidden var for loop
+    _type_correspondences = []
+
+    # dynamically construct corr dict
+    for i, x in enumerate(puzz_input_no_seed_info):
+        print(x)
+        if x in correspondence_types:
+            _corr_type = x
+
+        if re.match(string=x, pattern='\d+'):
+            _type_correspondences.append(x)
+        else:
+            print(f'finished type {_corr_type}')
+            correspondence_dict[_corr_type] = type_correspondences
+            type_correspondences = []
+
+
+    return correspondence_dict
+
+
+
+
+
+
+
